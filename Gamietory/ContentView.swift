f887dacd908ffe7671a36cad65fa12bddc3a09bb
laetuz <laetuzg@gmail.com>
2024-04-03 13:58:06 +0700

    Initial Commit

---
+++ Gamietory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gamietory
+//
+//  Created by Ryo Martin on 03/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gamietory/GamietoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamietoryApp.swift
+//  Gamietory
+//
+//  Created by Ryo Martin on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct GamietoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
